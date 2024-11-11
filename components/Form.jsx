fbee5c8ec3549eaa473a50feac2992cc3ad5bdc4
Miguel Morales <miguelmsaguilar@gmail.com>
2024-11-11 14:42:59 -0500

    se agrego boton de submit y se enlazo a un handlesubmit

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react"
-import { TextInput, View, Text } from "react-native";
+import { TextInput, View, Text, FlatList, Alert, Button } from "react-native";
+import APIService from "../services/APIService";
+import { obtenerProveedores } from "../lib/axios";
 
 const Form = () => {
-    const [text, setText] = useState('');
+    const [material, setMaterial] = useState('');
+    const [proveedores, setProveedores] = useState([]);
+
+    // async function obtenerProveedores() {
+    //     const {data} = await APIService.buscarProveedores(material);
+    //     proveedores.value = data
+    // }
+
+    const handleSubmit = () => {
+        alert(material)
+    }
 
     return(
         <View>
             <Text className="text-center uppercase font-bold mb-4 text-lg">Buscadoor de proveedores</Text>
             <TextInput 
+                id="material"
                 className="border border-black rounded p-2"
                 placeholder="Escriba el nombre del material o proveedor"
-                onChangeText={newText => setText(newText)}
-                defaultValue={text}
+                onChangeText={newMaterial => setMaterial(newMaterial)}
+                onSubmitEditing={handleSubmit}
             />
-            <Text>{text}</Text>
+            {/* <Text>{material}</Text> */}
+            <Button title="Submit" onPress={handleSubmit}/>
         </View>
     )
 }
